b84a2fddbe258263c1f85daacc89d3d01159a131
Brian Foo <brian@youaremyjoy.org>
2017-03-15 17:41:16 -0400

    first pass at getting streams

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const config = require('config');
+const config = require('config')
+const express = require('express')
+const getStreams = require('./lib/util').getStreams
 
-var express = require('express');
-var app = express();
+var app = express()
 
 // Enable static files
 app.use(express.static('public'))
 
 // Only allow GET requests
 app.all('*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
   next();
 });
 
 // Retrieve streams
 app.get('/streams', function (req, res) {
-  var resp = {
-    message: 'Hello, world'
-  }
-  res.type('application/json')
-  res.status(200).send(JSON.stringify(resp))
+  getStreams(config.get('streams')).then(function(promiseResponses){
+    res.type('application/json')
+    res.status(200).send(JSON.stringify(promiseResponses))
+  })
 });
 
 app.listen(3000)
